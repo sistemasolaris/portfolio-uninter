4a1843d0e87b97fc890cb45edb86c5ef95af29ac
Solaris <ovvo.system@gmail.com>
2024-03-31 20:25:12 -0300

    feat: create website

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleHamburgerMenu() {
+    var x = document.getElementById("navbar");
+
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+} 
